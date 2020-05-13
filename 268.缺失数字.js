f1bd6daa7378753bc263b0a5ddcae7c91c1a8e07
dubo <microcosm1994@gmail.com>
2020-05-13 09:28:52 +0800

    1

---
+++ 268.缺失数字.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=268 lang=javascript
+ *
+ * [268] 缺失数字
+ *
+ * https://leetcode-cn.com/problems/missing-number/description/
+ *
+ * algorithms
+ * Easy (55.36%)
+ * Likes:    253
+ * Dislikes: 0
+ * Total Accepted:    63.4K
+ * Total Submissions: 114.1K
+ * Testcase Example:  '[3,0,1]'
+ *
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ * 
+ * 
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    nums = nums.sort((a, b) => {
+        return a - b
+    })
+    for (let i = 0; i < nums.length; i++) {
+        if (i !== nums[i]) return i
+    }
+    return nums[nums.length - 1] + 1
+};
+missingNumber([45, 35, 38, 13, 12, 23, 48, 15, 44, 21, 43, 26, 6, 37, 1, 19, 22, 3, 11, 32, 4, 16, 28, 49, 29, 36, 33, 8, 9, 39, 46, 17, 41, 7, 2, 5, 27, 20, 40, 34, 30, 25, 47, 0, 31, 42, 24, 10, 14])
+// @lc code=end
+
